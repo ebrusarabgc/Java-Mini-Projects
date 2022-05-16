68470edb4b6eef2fb978bb978d79c272079cf9fe
Ebru Şara Bağca <ebru.sara123@outlook.com>
2022-05-16 14:26:59 +0300

    Initial commit

---
+++ src/SimpleATMProject/ATM.java
@@ -0,0 +1,77 @@
+package SimpleATMProject;
+
+import java.util.Scanner;
+
+public class ATM {
+
+    public void run(Account account) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the ATM program...");
+        System.out.println("User Login");
+
+        int rightOfEntry = 3;
+
+        while (true) {
+            if (!LogIn.logIn(account)) {
+                rightOfEntry--;
+                System.out.println("Wrong user name or password.");
+            } else {
+                System.out.println("Login successful.");
+                break;
+            } if (rightOfEntry == 0) {
+                System.out.println("Your remaining access rights have expired.");
+                return;
+            }
+            System.out.println("Your remaining access right is: " + rightOfEntry);
+        }
+
+        String operations = """
+                1. View Balance
+                2. Deposit Money
+                3. Withdraw Money
+                Press q to exit.""";
+
+        System.out.println();
+        System.out.println(operations);
+
+        label:
+        while (true) {
+            System.out.println("Select an operation to do: ");
+
+            String operation = scanner.next();
+
+            switch (operation) {
+                case "q":
+                    break label;
+                case "1":
+                    System.out.println("Your Balance: " + account.getBalance());
+                    break;
+                case "2": {
+                    System.out.print("Enter the amount value to deposit: ");
+
+                    int amount = scanner.nextInt();
+                    scanner.nextLine();
+
+                    account.depositMoney(amount);
+                    break;
+                }
+                case "3": {
+                    System.out.println("Enter the amount value to withdraw: ");
+                    int amount = scanner.nextInt();
+                    scanner.nextLine();
+
+                    account.withdrawMoney(amount);
+                    break;
+                }
+                default:
+                    System.out.println("Invalid operation...");
+                    break;
+            }
+
+        }
+
+    }
+
+}
---
+++ src/SimpleATMProject/Main.java
@@ -0,0 +1,12 @@
+package SimpleATMProject;
+
+public class Main {
+    public static void main(String[] args) {
+        ATM atm = new ATM();
+        Account account1 = new Account("Ebru Bağca", 2000, "password");
+
+        atm.run(account1);
+
+        System.out.println("Exiting ATM program...");
+    }
+}
