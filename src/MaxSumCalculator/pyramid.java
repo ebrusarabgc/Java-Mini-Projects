9b73829ab4fd05a0f19a0c102fb588b0ebc713e1
Ebru Şara Bağca <ebru.sara123@outlook.com>
2022-06-01 12:22:08 +0300

    Edited so that input can be taken

--- src/MaxSumCalculator/pyramid.java
+++ src/MaxSumCalculator/pyramid.java
@@ -1,5 +1,7 @@
 package MaxSumCalculator;
 
+import java.util.Scanner;
+
 public class pyramid {
     // Finding maximum sum of NON-prime numbers in a pyramid
 
@@ -20,25 +22,25 @@ public class pyramid {
     }
 
     public static void main(String[] args) {
-        int[][] pyramid = {
-                {215},
-                {193, 124},
-                {117, 237, 442},
-                {218, 935, 347, 235},
-                {320, 804, 522, 417, 345},
-                {229, 601, 723, 835, 133, 124},
-                {248, 202, 277, 433, 207, 263, 257},
-                {359, 464, 504, 528, 516, 716, 871, 182},
-                {461, 441, 426, 656, 863, 560, 380, 171, 923},
-                {381, 348, 573, 533, 447, 632, 387, 176, 975, 449},
-                {223, 711, 445, 645, 245, 543, 931, 532, 937, 541, 444},
-                {330, 131, 333, 928, 377, 733, 17, 778, 839, 168, 197, 197},
-                {131, 171, 522, 137, 217, 224, 291, 413, 528, 520, 227, 229, 928},
-                {223, 626, 34, 683, 839, 53, 627, 310, 713, 999, 629, 817, 410, 121},
-                {924, 622, 911, 233, 325, 139, 721, 218, 253, 223, 107, 233, 230, 124, 233}};
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(maxSum(pyramid,0,0,15,15));
+        String str = scanner.nextLine();
+        String[] arr = str.split(" ");
 
+        int a = (int) (Math.sqrt(2 * arr.length) - 1/2);
+        int[][] pyramid = new int[a][a];
+        int count = 0;
+
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j <= i; j++) {
+                pyramid[i][j] = Integer.parseInt(arr[count]);
+                count++;
+            }
+
+        }
+
+        System.out.println(maxSum(pyramid,0,0,a,a));
     }
 
     public static boolean isPrime(int num) {
